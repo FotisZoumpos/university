bd7953d0ca9e870a717134e631a328c067823c57
FotisZoumpos <fzoump1993@gmail.com>
2024-07-27 15:48:08 +0300

    Cleared up everything in Main , ProfessorService and ProfessorRepository. Starting again

--- src/main/java/com/uni/university/repository/ProfessorRepository.java
+++ src/main/java/com/uni/university/repository/ProfessorRepository.java
@@ -1,7 +1,6 @@
 package com.uni.university.repository;
 
 import com.uni.university.domain.Professor;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,16 +9,4 @@ import org.springframework.stereotype.Repository;
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 
 
-  void deleteAll();
-
-  List<Professor> findByFirstName(String firstName);
-
-  List<Professor> findByLastName(String lastName);
-
-  Professor findByPhone(String phone);
-
-
-  boolean existsByEmail(String email);
-
-  boolean existsByPhone(String phone);
 }
--- src/main/java/com/uni/university/service/ProfessorService.java
+++ src/main/java/com/uni/university/service/ProfessorService.java
@@ -1,11 +1,6 @@
 package com.uni.university.service;
 
-import com.uni.university.domain.Course;
-import com.uni.university.domain.Professor;
 import com.uni.university.repository.ProfessorRepository;
-import jakarta.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,73 +8,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ProfessorService {
   private final ProfessorRepository repository;
-
-
-  public Professor findByPhone(String phone) {
-    if (repository.findByPhone(phone) != null) {
-      System.out.println("Phone " + phone + " is found.");
-      return repository.findByPhone(phone);
-    }
-    System.out.println("Invalid phone number.");
-    return null;
-  }
-
-  public List<Professor> findByFirstName(String firstName) {
-    return repository.findByFirstName(firstName);
-  }
-
-  public List<Professor> findByLastName(String lastName) {
-    return repository.findByLastName(lastName);
-  }
-
-  @Transactional
-  public void update(Professor professor) {
-
-    repository.save(professor);
-  }
-
-  public List<Professor> findAll() {
-    return repository.findAll();
-  }
-
-  public Optional<Professor> findById(Long id) {
-
-    return repository.findById(id);
-
-  }
-
-
-  @Transactional
-  public Optional<Professor> deleteById(long id) {
-    Professor professor = repository.findById(id).orElse(null);
-    if (professor != null) {
-      List<Course> courses = professor.getCourses();
-
-      for (Course course : courses) {
-        course.setProfessor(null);
-      }
-      repository.delete(professor);
-      System.out.println("Professor deleted: " + professor);
-      return Optional.of(professor);
-    } else {
-      System.out.println("Professor with ID:" + id + " not found.");
-      return Optional.empty();
-
-    }
-  }
-
-  @Transactional
-  public void deleteAll() {
-    List<Professor> professors = findAll();
-    for (Professor professor : professors) {
-      List<Course> courses = professor.getCourses();
-      for (Course course : courses) {
-        course.setProfessor(null);
-      }
-      repository.delete(professor);
-    }
-    System.out.println("Professors deleted successfully.");
-  }
 
 
 }
