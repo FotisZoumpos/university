5bf8f3e9c2bae728479b716c19d21e6eddd82485
FotisZoumpos <fzoump1993@gmail.com>
2024-07-27 15:59:56 +0300

    Added methods findAll and findById

--- src/main/java/com/uni/university/service/ProfessorService.java
+++ src/main/java/com/uni/university/service/ProfessorService.java
@@ -1,6 +1,9 @@
 package com.uni.university.service;
 
+import com.uni.university.domain.Professor;
 import com.uni.university.repository.ProfessorRepository;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +12,16 @@ import org.springframework.stereotype.Service;
 public class ProfessorService {
   private final ProfessorRepository repository;
 
+
+  public List<Professor> findAll() {
+    return repository.findAll();
+  }
+
+  public Optional<Professor> findById(Long id) {
+
+    return repository.findById(id);
+
+  }
 
 }
 
