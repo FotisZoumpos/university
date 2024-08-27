fa01308909c1de9cb2cea0264b9d6a3e3c73775d
Christoforos Kelaidis <christoforos.kelaidis@hoopcare.com>
2024-08-27 21:58:20 +0300

    Remove redundant code from ProfessorService

--- src/main/java/com/uni/university/service/ProfessorService.java
+++ src/main/java/com/uni/university/service/ProfessorService.java
@@ -1,19 +1,15 @@
 package com.uni.university.service;
 
-import com.uni.university.constraints.CreateGroup;
 import com.uni.university.domain.Professor;
 import com.uni.university.dto.CreateUpdateProfessorDto;
 import com.uni.university.repository.ProfessorRepository;
-import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 
 @Service
 @RequiredArgsConstructor
-@Validated
 public class ProfessorService {
 
   private final ProfessorRepository repository;
@@ -30,64 +26,35 @@ public class ProfessorService {
   }
 
   public Professor save(Professor professor) {
+    /*TODO could save implement the existsByUsername condition instead of the create?
+     *  Practically we want to check if the username exists during the save
+     *  two conditions:
+     *  1. new professor
+     *  2. existing professor*/
+
     return repository.save(professor);
   }
 
-
   @SneakyThrows
-  @Validated(CreateGroup.class)
-  public Professor create(@Valid CreateUpdateProfessorDto professorDto) {
-
+  public Professor create(CreateUpdateProfessorDto professorDto) {
     if (repository.existsByUsername(professorDto.getUsername())) {
       throw new Exception();
     }
 
-    Professor professor = convertToProfessor(professorDto);
-
-    return repository.save(professor);
+    return repository.save(convertToProfessor(professorDto));
   }
 
   @SneakyThrows
   public Professor update(CreateUpdateProfessorDto professorDto) {
-   
-    Professor existingProfessor = findOrThrow(professorDto.getId());
-    if (professorDto.getFirstName() != null && !professorDto.getFirstName().equals(existingProfessor.getFirstName())) {
-      existingProfessor.setFirstName(professorDto.getFirstName());
-    } else {
-      throw new Exception();
-    }
-    if (professorDto.getLastName() != null && !professorDto.getLastName().equals(existingProfessor.getLastName())) {
-      existingProfessor.setLastName(professorDto.getLastName());
-    } else {
-      throw new Exception();
-    }
-    if (professorDto.getUsername() != null && !professorDto.getUsername().equals(existingProfessor.getUsername())) {
-      existingProfessor.setUsername(professorDto.getUsername());
-    } else {
-      throw new Exception();
-    }
-    if (professorDto.getEmail() != null && !professorDto.getEmail().equals(existingProfessor.getEmail())) {
-      existingProfessor.setEmail(professorDto.getEmail());
-    } else {
-      throw new Exception();
-    }
-    if (professorDto.getPhone() != null && !professorDto.getPhone().equals(existingProfessor.getPhone())) {
-      existingProfessor.setPhone(professorDto.getPhone());
-    } else {
-      throw new Exception();
-    }
-    if (professorDto.getGender() != null && !professorDto.getGender().equals(existingProfessor.getGender())) {
-      existingProfessor.setGender(professorDto.getGender());
-    } else {
-      throw new Exception();
-    }
-    if (professorDto.getBirthday() != null && !professorDto.getBirthday().equals(existingProfessor.getBirthday())) {
-      existingProfessor.setBirthday(professorDto.getBirthday());
-    } else {
-      throw new Exception();
-    }
+    var professor = findOrThrow(professorDto.getId());
+    professor.setFirstName(professorDto.getFirstName());
+    professor.setLastName(professorDto.getLastName());
+    professor.setEmail(professorDto.getEmail());
+    professor.setPhone(professorDto.getPhone());
+    professor.setGender(professorDto.getGender());
+    professor.setBirthday(professorDto.getBirthday());
 
-    return save(existingProfessor);
+    return save(professor);
   }
 
   public void deleteById(Long id) {
@@ -102,7 +69,8 @@ public class ProfessorService {
 
   @SneakyThrows
   public Professor convertToProfessor(CreateUpdateProfessorDto professorDto) {
-    Professor professor = new Professor();
+    // TODO Add Mapstruct for this
+    var professor = new Professor();
     professor.setId(professorDto.getId());
     professor.setFirstName(professorDto.getFirstName());
     professor.setLastName(professorDto.getLastName());
@@ -113,20 +81,4 @@ public class ProfessorService {
     professor.setBirthday(professorDto.getBirthday());
     return professor;
   }
-
-  public CreateUpdateProfessorDto convertToProfessorDto(Professor professor) {
-    CreateUpdateProfessorDto professorDto = new CreateUpdateProfessorDto();
-    professorDto.setId(professorDto.getId());
-    professorDto.setFirstName(professor.getFirstName());
-    professorDto.setLastName(professor.getLastName());
-    professorDto.setUsername(professor.getUsername());
-    professorDto.setEmail(professor.getEmail());
-    professorDto.setPhone(professor.getPhone());
-    professorDto.setBirthday(professor.getBirthday());
-    professorDto.setGender(professor.getGender());
-
-    return professorDto;
-  }
-
-
 }
